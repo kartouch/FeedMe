6d299e54e13c5159d4c037384d2fd21c04aefe10
Christophe Augello <christophe@augello.be>
2016-01-22 14:05:22 +0100

    fix

--- app.rb
+++ app.rb
@@ -12,25 +12,24 @@ class FeedMe < Sinatra::Base
   set :json_encoder, :to_json
   set :json_content_type, 'application/json;charset=utf-8'
 
-@redis = Redis.new(host: "#{ENV['REDIS_SERVICE_HOST']}")
 
 get '/' do
-			@redis = Redis.new(host: "#{ENV['REDIS_SERVICE_HOST']}")
+  @redis = Redis.new(host: "#{ENV['REDIS_SERVICE_HOST']}")
      if @redis.get("feeds").nil?
       @redis.set("feeds", Feed.find_by_sql("select * from feeds order by random() limit 3000").to_json)
       @redis.expire("feeds",300)
-      @feeds = JSON.parse(redis.get("feeds"), object_class: OpenStruct)
+      @feeds = JSON.parse(@redis.get("feeds"), object_class: OpenStruct)
       erb :index
     else
       @redis.get("feeds")
-      @feeds = JSON.parse(redis.get("feeds"), object_class: OpenStruct)
+      @feeds = JSON.parse(@redis.get("feeds"), object_class: OpenStruct)
       @feeds = Feed.find_by_sql("select * from feeds order by random() limit 3000")
       erb :index
     end
  end
 
  get '/feeds' do
-			@redis = Redis.new(host: "#{ENV['REDIS_SERVICE_HOST']}")
+ @redis = Redis.new(host: "#{ENV['REDIS_SERVICE_HOST']}")
     if @redis.get("feeds").nil?
       @redis.set("feeds", Feed.find_by_sql("select * from feeds order by random() limit 3000").to_json)
       @redis.expire("feeds",300)
